a27521870ec1fac13ef55c0cc9d6bd9be200e742
haeni0117 <zion0117@ewhain.net>
2021-09-20 10:48:40 +0900

    clear github posts

--- _posts/BOJ 문제풀이/1427소트인사이드.py
+++
@@ -1,5 +0,0 @@
-n = input()#처음부터 int형으로 받으면 2134가 이천백삼십사라고 인식함 
-num = [int(i) for i in n]#문자열 하나씩 가져오기 -> 숫자분리
-answer = num.sorted(num,reverse=True) #1234-> 4321 역순으로 순서설정
-#출력
-print(int(i) for i in answer
--- _posts/BOJ 문제풀이/2667(1).py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-s = [[0]*n for _ in range(n)]
-visited = [[0]*n for _ in range(n)]
-
-for i in range(n):
-    s[i] = map(int,input().split())
